d33d8f6ea18291910d9e35ed1f5b877ca413a8b5
Janis <profiskiller007@gmail.com>
2020-04-13 16:28:09 +0200

    Changed Utils.java

--- src/main/java/de/demokratie/polizeibot/utils/Utils.java
+++ src/main/java/de/demokratie/polizeibot/utils/Utils.java
@@ -1,30 +1,35 @@
 package de.demokratie.polizeibot.utils;
 
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Utils {
 
-    public static File warnsFile = new File("warns.yml");
-    public static File mutesFile = new File("mutes.yml");
-
-    public static YamlConfiguration warns = YamlConfiguration.loadConfiguration(warnsFile);
-    public static YamlConfiguration mutes = YamlConfiguration.loadConfiguration(mutesFile);
-
     public static void warn(Member m, String reason) {
+        try {
+            File f = new File("users/" + m.getId() + "/");
+            if(f.exists()) {
+                f.createNewFile();
+            }
+            YamlConfiguration c = YamlConfiguration.loadConfiguration(f);
+            if(c.contains(m.getId())) {
+                int warns = c.getInt(m.getId() + ".warns");
+                c.set(m.getId() + ".warns", warns + 1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public static void mute(GuildMessageReceivedEvent e, Member m, String reason) {
-
     }
 
     public static void tempmute(GuildMessageReceivedEvent e, Member m, String reason, int days) {
-
     }
 
 }
