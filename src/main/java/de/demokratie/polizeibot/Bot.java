9c570ad3833ddef12e5bb164b3fd17978ea7d571
cephox <siopsalm@gmail.com>
2020-04-13 15:12:04 +0200

    Created Bot and added Compiler Version

--- src/main/java/de/demokratie/polizeibot/Bot.java
+++ src/main/java/de/demokratie/polizeibot/Bot.java
@@ -4,32 +4,6 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
-/*****************************************************************************
- *                                                                           *
- *  Urheberrechtshinweis:                                                    *
- *   Copyright © Paul Stier, 2020                                            *
- *   Erstellt: 13.04.2020 / 15:00                                            *
- *                                                                           *
- *   Alle Inhalte dieses Quelltextes sind urheberrechtlich geschützt.        *
- *   Das Urheberrecht liegt, soweit noch ausdrücklich anders gekennzeichnet, *
- *   bei Paul Stier. Alle Rechte vorbehalten.                                *
- *                                                                           *
- *   Jede Art von Vervielfältigung, Verbreitung, Vermietung, Verleihung,     *
- *   öffentliche Zugänglichmachung, Veränderung oder andere Nutzung          *
- *   bedarf der ausdrücklichen, schriftlichen Zustimmung von Paul Stier.     *
- *                                                                           *
- *****************************************************************************/
-
-/*
-
-    Class created by ce_phox on
-    13.04.2020 15:00.
-    
-    Discord: ce_phox#1259
-
-*/
-
-
 public class Bot {
 
     public JDA jda;
@@ -38,7 +12,7 @@ public class Bot {
 
         try {
             jda = new JDABuilder(AccountType.BOT)
-                    .setToken("Njk5MjQyNjk5MDk3ODk5MTE4.XpRitQ.rrEmpeJ7Xufe0bTt9jvwRLiqNEs")
+                    .setToken("TOKEN")
                     .setAutoReconnect(true)
                     .build().awaitReady();
         } catch (Exception ex) {
