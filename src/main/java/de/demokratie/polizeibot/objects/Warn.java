e419333995bedd18f17e62b0789f2763bb514f38
Janis <profiskiller007@gmail.com>
2020-04-13 20:13:54 +0200

    Added Object Warn.java Edited Utils.java

---
+++ src/main/java/de/demokratie/polizeibot/objects/Warn.java
@@ -0,0 +1,53 @@
+package de.demokratie.polizeibot.objects;
+
+import net.dv8tion.jda.api.entities.Member;
+
+import java.util.Date;
+
+public class Warn {
+
+    String reason;
+    Member m;
+    Member warner;
+    Date d;
+
+    public void Warns(String reason, Member m, Member warner, Date d) {
+        this.reason = reason;
+        this.m = m;
+        this.warner = warner;
+        this.d = d;
+    }
+
+    public Date getDate() {
+        return d;
+    }
+
+    public Member getMember() {
+        return m;
+    }
+
+    public Member getWarner() {
+        return warner;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setDate(Date d) {
+        this.d = d;
+    }
+
+    public void setMember(Member m) {
+        this.m = m;
+    }
+
+    public void setWarner(Member warner) {
+        this.warner = warner;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+}
--- src/main/java/de/demokratie/polizeibot/utils/Utils.java
+++ src/main/java/de/demokratie/polizeibot/utils/Utils.java
@@ -1,25 +1,33 @@
 package de.demokratie.polizeibot.utils;
 
+import de.demokratie.polizeibot.objects.Warn;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class Utils {
 
-    public static void warn(Member m, String reason) {
+    public static void warn(Member m, String reason, Member warner) {
         try {
-            File f = new File("users/" + m.getId() + "/");
+            File d = new File("users/" + m.getId());
+            File f = new File("users/" + m.getId() + "/warns.yml");
+            if(!d.exists()) {
+                d.mkdir();
+            }
             if (!f.exists()) {
                 f.createNewFile();
             }
             YamlConfiguration c = YamlConfiguration.loadConfiguration(f);
-            if(c.contains(m.getId())) {
-                int warns = c.getInt(m.getId() + ".warns");
-                c.set(m.getId() + ".warns", warns + 1);
-            }
+            int warns = c.getInt("warns");
+            List<String> reasons = new ArrayList<>();
+            reasons.add(reason);
+            c.set("reasons", reasons);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,6 +38,28 @@ public class Utils {
     }
 
     public static void tempmute(GuildMessageReceivedEvent e, Member m, String reason, int days) {
+    }
+
+    public static List<Warn> getWarns(Member m, GuildMessageReceivedEvent e) {
+        List<Warn> warns = new ArrayList<>();
+        File f = new File("users/" + m.getId() + "/warns.yml");
+        if(f.exists()) {
+            YamlConfiguration c = YamlConfiguration.loadConfiguration(f);
+            List<String> reasons = c.getStringList("reasons");
+            reasons.stream().forEach((reason) -> {
+                Warn warn = new Warn();
+                warn.setDate(new Date());
+                warn.setMember(m);
+                Member warner = e.getGuild().getMemberById(reason + ".warner");
+                warn.setWarner(warner);
+                warn.setReason(reason);
+                warns.add(warn);
+            });
+        }
+        else {
+            return warns;
+        }
+        return warns;
     }
 
 }
