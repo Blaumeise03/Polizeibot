9691a647d5fdb668c67c9858aa6308738988c04d
Janis <profiskiller007@gmail.com>
2020-04-14 15:03:09 +0200

    Edited Utils.java Edited Mute.java

--- src/main/java/de/demokratie/polizeibot/objects/Mute.java
+++ src/main/java/de/demokratie/polizeibot/objects/Mute.java
@@ -8,8 +8,6 @@ public class Mute {
 
     private boolean muted;
 
-
-
     private String reason;
     private String type;
     private Member m;
--- src/main/java/de/demokratie/polizeibot/utils/Utils.java
+++ src/main/java/de/demokratie/polizeibot/utils/Utils.java
@@ -206,7 +206,6 @@ public class Utils {
             mute.setMuted(info.isMuted());
             info.setMute(mute);
         }
-
         return info;
     }
 
